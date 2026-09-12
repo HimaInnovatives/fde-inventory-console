9c24dce5217e78967101f5c610be14fac4db7e3f
Himavarshini <yaleruhimavarshini09@gmail.com>
2026-09-12 11:56:34 +0530

    Clean up repo: remove crash logs, committed jar, compiled output, and old prototype files

--- inventory-system/src/com/fde/Item.java
+++
@@ -1,8 +0,0 @@
-package com.fde;
-public class Item {
-    private int id;
-    private String name;
-    public Item(int id, String name) { this.id = id; this.name = name; }
-    public int getId() { return id; }
-    public String getName() { return name; }
-}
